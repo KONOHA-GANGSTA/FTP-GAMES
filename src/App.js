f5746e759725261a7cb88547e0939a7baadba57a
KONOHA_GANGSTA <piligrim507@gmail.com>
2023-09-18 11:38:17 +0300

    СКЕЛЕТОНЫ

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import appStyles from "./App.css";
 import { GameGrid } from "./GameGrid/GameGrid";
 import { Header } from "./Header/Header";
+import { CardSkeleton } from "./CardSkeleton/CardSkeleton";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className={appStyles.app}>
-      <Header str="FREE TO PLAY" />
+      {/* <CardSkeleton /> */}
+      {/* <Header str="FREE TO PLAY" /> */}
       <GameGrid />
     </div>
+    </ThemeProvider>
   );
 }
 
--- src/GameGrid/GameGrid.jsx
+++ src/GameGrid/GameGrid.jsx
@@ -2,9 +2,16 @@ import { Grid } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { fetchData } from "../functions/fetchData";
 import { GameCard } from "../GameCard/GameCard";
+import { CardSkeleton } from "../CardSkeleton/CardSkeleton";
 
 export function GameGrid() {
   const [games, setGames] = useState([]);
+
+  const getArr = (num) => {
+    let result = [];
+    for (let i = 0; i < num; ++i) result.push(0);
+    return result;
+  };
 
   useEffect(() => {
     fetchData(games, setGames);
@@ -12,11 +19,17 @@ export function GameGrid() {
 
   return (
     <Grid container spacing={2}>
-      {games.map((game, index) => (
-        <Grid lg={3} xs={12} sm={4} item key={index}>
-          <GameCard game={game} />
-        </Grid>
-      ))}
+      {games[0]
+        ? games.map((game, index) => (
+            <Grid lg={3} xs={12} sm={4} item key={index}>
+              <GameCard game={game} />
+            </Grid>
+          ))
+        : getArr(20).map((el, ind) => (
+            <Grid lg={3} xs={12} sm={4} item key={ind}>
+              <CardSkeleton />
+            </Grid>
+          ))}
     </Grid>
   );
 }
